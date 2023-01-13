9f11cb3552ba3f7f345c5ee4e147cb049dda56ca
Sourav Kantha <souravkantha@Souravs-MacBook-Air.local>
2023-01-13 23:36:43 +0530

    added methods for binary heap

--- src/heaps/BinaryHeap.java
+++ src/heaps/BinaryHeap.java
@@ -1,5 +1,26 @@
 package heaps;
 
 public class BinaryHeap {
+	
+	Integer [] heap = null;
+	
+	public void buildMaxHeap(Integer [] arr) {
+		
+		
+	}
+	
+	public Integer getMax() {
+		
+		return null;
+		
+	}
+	
+	
+	private void maxHeapify(Integer [] arr, Integer i) {
+		
+		
+	}
+	
+	
 
 }
